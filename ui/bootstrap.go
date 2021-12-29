3b8e7737e05e71301586676db45a1ee6c3a7291f
PJ Quirk <pjquirk@github.com>
2021-12-29 13:44:43 -0500

    Make the author field constant width

--- ui/bootstrap.go
+++ ui/bootstrap.go
@@ -58,24 +58,35 @@ func (m BootstrapModel) Update(msg tea.Msg) (BootstrapModel, tea.Cmd) {
 		case cmd.PullRequestsFetched:
 			fetched := cmd.PullRequestsFetched(msg)
 			m.pullRequests = fetched.PullRequests
+			longestAuthor := 0
 
 			data := make([]interface{}, len(m.pullRequests))
 			for i, v := range m.pullRequests {
+				longestAuthor = max(longestAuthor, len(v.Author))
 				data[i] = v
 			}
+
+			// Make the author field constant width
+			prFormatStr := fmt.Sprintf("@%%-%ds - %%s", longestAuthor)
+			selectedFormatStr := fmt.Sprintf("[%%d] %s", prFormatStr)
+			unSelectedFormatStr := fmt.Sprintf(" %%d. %s", prFormatStr)
+
 			m.prSelector = &selector.Model{
 				Data:       data,
 				PerPage:    5,
 				HeaderFunc: selector.DefaultHeaderFuncWithAppend("Select a pull request to split:"),
 				// [1] The title of the pull request (@author)
-				SelectedFunc: func(m selector.Model, obj interface{}, index int) string {
+				SelectedFunc: func(_ selector.Model, obj interface{}, _ int) string {
 					pr := obj.(cmd.PullRequest)
-					return common.FontColor(fmt.Sprintf("[%d] %s (@%s)", pr.Number, pr.Title, pr.Author), selector.ColorSelected)
+					return common.FontColor(fmt.Sprintf(selectedFormatStr, pr.Number, pr.Author, pr.Title), selector.ColorSelected)
 				},
 				//  1. The title of the pull request (@author)
-				UnSelectedFunc: func(m selector.Model, obj interface{}, index int) string {
+				UnSelectedFunc: func(_ selector.Model, obj interface{}, _ int) string {
 					pr := obj.(cmd.PullRequest)
-					return common.FontColor(fmt.Sprintf(" %d. %s (%s)", pr.Number, pr.Title, pr.Author), selector.ColorUnSelected)
+					return common.FontColor(fmt.Sprintf(unSelectedFormatStr, pr.Number, pr.Author, pr.Title), selector.ColorUnSelected)
+				},
+				FooterFunc: func(_ selector.Model, _ interface{}, _ int) string {
+					return common.FontColor(fmt.Sprintf("%d pull requests found", len(data)), selector.ColorFooter)
 				},
 			}
 		}
@@ -103,4 +114,11 @@ func (m BootstrapModel) View() string {
 	} else {
 		return m.prSelector.View()
 	}
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
