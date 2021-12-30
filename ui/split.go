e5b7442b49def168b3d25785281876f2698e17ee
PJ Quirk <pjquirk@github.com>
2021-12-30 14:48:50 -0500

    Notes

--- ui/split.go
+++ ui/split.go
@@ -48,6 +48,27 @@ func (m SplitModel) View() string {
 		return ""
 	}
 
+	/* To perform the split, we need a working copy.  We have a few options:
+	 * 1. Require the working directory to match the selected repository.
+	 *    - We could remove the repository CLI option.
+	 *    - We need to fetch latest
+	 *    - May hit issues if the working dir has changes in it
+	 * 2. Clone the repository into a temp directory
+	 *    - gh repo clone <nwo> <tmp dir>
+	 *    - Delete the directory afterwards
+	 *
+	 * Once we have a working copy with any branch checked out:
+	 * - git checkout -b <branch name> <same base branch as original branch>
+	 * - git cherry-pick sha1 sha2 sha3 sha4
+	 * - Write PR body to a temp file
+	 * - gh pr create --base <original base> --head <branch name> --title <PR title> --body-file <temp file with body>
+	 * - Delete body file
+	 *
+	 * Potential problems:
+	 * - The chosen commits may not apply cleanly
+	 * - Need to check the remote name
+	 */
+
 	//return m.prSelector.View()
 	return "Here wo go!"
 }
